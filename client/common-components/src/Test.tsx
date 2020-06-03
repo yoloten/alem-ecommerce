5c1f59f43117c12d233b34a53ca41f076365585a
yoloten <rustamsahatov50@gmail.com>
2020-06-03 19:24:21 +0300

    common-components settings donr

---
+++ client/common-components/src/Test.tsx
@@ -0,0 +1,5 @@
+import * as React from "react"
+
+export function Test(): JSX.Element {
+    return <div>вы</div>
+}
--- client/spa-app/src/index.tsx
+++ client/spa-app/src/index.tsx
@@ -1,6 +1,11 @@
 import * as ReactDOM from "react-dom"
+import * as UI from "../../common-components/src/"
 import React from "react"
 
-const root = <div>adsda</div>
+const root = (
+    <div>
+        <UI.Test />
+    </div>
+)
 
 ReactDOM.render(root, document.getElementById("root"))
